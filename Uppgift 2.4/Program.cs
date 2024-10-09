63a176646850b6f403ffe19bb323439278a3e178
alsa07007 <alsa07007@E-PW05CGR5>
2024-10-09 12:46:29 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Räkna ut medellön");
+            Console.WriteLine("Ange lön för anställd1:");
+            double l1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ange lön för anställd2:");
+            double l2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ange lön för anställd3:");
+            double l3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Medellön: {(l1 + l2 + l3) / 3}");
+        }
+    }
+}
